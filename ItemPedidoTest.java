45bf5d761edea78f6924f7abda6a80252ec8f47d
kaps1024 <kapsseind@gmail.com>
2026-05-18 20:44:50 -0300

    Create ItemPedidoTest.java

---
+++ ItemPedidoTest.java
@@ -0,0 +1,11 @@
+public class ItemPedidoTest {
+
+    public void verificarItemPedido() {
+
+        ItemPedido item = new ItemPedido();
+
+        System.out.println(
+            item.calcularSubtotal()
+        );
+    }
+}
