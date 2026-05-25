0d870a0c8f34ec2a9d28f67d5223d6819f06d6e5
kaps1024 <kapsseind@gmail.com>
2026-05-25 20:18:50 -0300

    Create Fornecedor.java

---
+++ Fornecedor.java
@@ -0,0 +1,12 @@
+public class Fornecedor {
+
+    private String nome;
+    private String cnpj;
+
+    public void fornecerProduto() {
+
+        System.out.println(
+            "Fornecedor fornecendo produtos."
+        );
+    }
+}
