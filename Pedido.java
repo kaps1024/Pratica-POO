875dfcafe5deaa0688aad0c81b4bfe9f3ea8be9b
kaps1024 <kapsseind@gmail.com>
2026-05-25 20:19:10 -0300

    Create Pedido.java

---
+++ Pedido.java
@@ -0,0 +1,12 @@
+public class Pedido {
+
+    private int numero;
+    private String data;
+
+    public void finalizarPedido() {
+
+        System.out.println(
+            "Pedido finalizado."
+        );
+    }
+}
