d1d93a5b4393de1e6f38e5369f85986d64db5869
kaps1024 <kapsseind@gmail.com>
2026-05-25 20:16:04 -0300

    Create Produto.java

---
+++ Produto.java
@@ -0,0 +1,46 @@
+public abstract class Produto {
+
+    private int codigo;
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double calcularValorEstoque() {
+
+        return preco * quantidade;
+    }
+
+    public abstract void exibirInfo();
+}
